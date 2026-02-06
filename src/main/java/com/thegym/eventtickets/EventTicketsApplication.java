3e8f418430edef5cd265e6ba616544de9f606e37
manzitresor <manzitre@gmail.com>
2026-02-06 12:18:24 +0200

    Project setup

---
+++ src/main/java/com/thegym/eventtickets/EventTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.thegym.eventtickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thegym/eventtickets/EventTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegym.eventtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
