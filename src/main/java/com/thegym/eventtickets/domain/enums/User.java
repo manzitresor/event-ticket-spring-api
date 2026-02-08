b64e31836dfc8f4be70efe98dee952c8c217825b
manzitresor <manzitre@gmail.com>
2026-02-08 22:40:59 +0200

    chore: Create User entity

--- src/main/java/com/thegym/eventtickets/domain/enums/User.java
+++ src/main/java/com/thegym/eventtickets/domain/enums/User.java
@@ -1,4 +1,36 @@
 package com.thegym.eventtickets.domain.enums;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false,updatable = false)
+    private UUID id;
+
+    @Column(name = "name", unique = true, nullable = false,updatable = false)
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @CreatedDate
+    @Column(name ="created_at", nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at",nullable = false,updatable = true)
+    private LocalDateTime updatedAt;
 }
