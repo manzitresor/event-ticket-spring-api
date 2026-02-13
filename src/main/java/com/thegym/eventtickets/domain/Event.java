c45c4c19f193084a76a59ec3229f70e8b2a094a3
manzitresor <manzitre@gmail.com>
2026-02-13 10:35:56 +0200

    chore: create Tickettype table and relashionship with event

--- src/main/java/com/thegym/eventtickets/domain/Event.java
+++ src/main/java/com/thegym/eventtickets/domain/Event.java
@@ -57,6 +57,9 @@ public class Event {
     private List<User> staff = new ArrayList<>();
 
 
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private List<TicketType> ticketTypes = new ArrayList<>();
+
     @CreatedDate
     @Column(name ="created_at", nullable = false,updatable = false)
     private LocalDateTime createdAt;
---
+++ src/main/java/com/thegym/eventtickets/domain/TicketType.java
@@ -0,0 +1,47 @@
+package com.thegym.eventtickets.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket-types")
+public class TicketType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "total_available")
+    private Integer totalAvailable;
+
+    @Column(name = "price",nullable = false)
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+
+    @CreatedDate
+    @Column(name ="created_at", nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at",nullable = false,updatable = true)
+    private LocalDateTime updatedAt;
+}
