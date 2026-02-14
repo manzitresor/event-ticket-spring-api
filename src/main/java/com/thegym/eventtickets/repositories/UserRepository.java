ad9f5b455131e4c8d92270becdec64588d2304d9
manzitresor <manzitre@gmail.com>
2026-02-14 20:26:58 +0200

    chore: create user repository

---
+++ src/main/java/com/thegym/eventtickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.thegym.eventtickets.repositories;
+
+import com.thegym.eventtickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
