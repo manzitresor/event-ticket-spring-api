5c10bb7f3bea7b56ea6a4ccbf29ab7f3bc48083a
manzitresor <manzitre@gmail.com>
2026-04-16 19:53:10 +0200

    chore: add constraint exceptional

--- src/main/java/com/thegym/eventtickets/domain/dtos/ErrorDto.java
+++ src/main/java/com/thegym/eventtickets/domain/dtos/ErrorDto.java
@@ -2,9 +2,11 @@ package com.thegym.eventtickets.domain.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class ErrorDto {
     private String error;
 }
--- src/main/java/com/thegym/eventtickets/exceptions/GlobalException.java
+++ src/main/java/com/thegym/eventtickets/exceptions/GlobalException.java
@@ -1,16 +1,33 @@
 package com.thegym.eventtickets.exceptions;
 
 import com.thegym.eventtickets.domain.dtos.ErrorDto;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalException {
 
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<ErrorDto> handleConstraintViolation(
+            ConstraintViolationException ex
+    ){
+        log.error("Validation error: ", ex);
 
+        ErrorDto errorDto = new ErrorDto();
+        String errorMessage = ex.getConstraintViolations().stream().findFirst().map(violation -> violation.getPropertyPath() + ": " + violation.getMessage()).orElse("Validation error occurred");
+        errorDto.setError(errorMessage);
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    @ExceptionHandler({Exception.class})
     public ResponseEntity<ErrorDto> handleException(Exception ex) {
         log.error("An error occurred: ", ex);
         ErrorDto errorDto = new ErrorDto("An unexpected error occurred. Please try again later.");
