c60458e206b881188d99a55d69761d2aea9fbdc4
manzitresor <manzitre@gmail.com>
2026-04-16 18:20:55 +0200

    chore: add Event mapper

---
+++ src/main/java/com/thegym/eventtickets/mappers/EventMapper.java
@@ -0,0 +1,20 @@
+package com.thegym.eventtickets.mappers;
+
+
+import com.thegym.eventtickets.domain.CreateEventRequest;
+import com.thegym.eventtickets.domain.CreateTicketTypeRequest;
+import com.thegym.eventtickets.domain.dtos.CreateEventRequestDto;
+import com.thegym.eventtickets.domain.entities.Event;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface EventMapper {
+
+    CreateEventRequest fromDto (CreateEventRequestDto dto);
+
+    CreateTicketTypeRequest fromDto (CreateTicketTypeRequest dto);
+
+    CreateEventRequestDto toDto (Event request);
+
+}
