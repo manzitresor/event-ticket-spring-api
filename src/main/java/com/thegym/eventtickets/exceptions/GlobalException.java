afb604767596fc54294d13be4869ec3bf292030a
manzitresor <manzitre@gmail.com>
2026-04-19 21:00:18 +0200

    chore: add method argument exception

--- src/main/java/com/thegym/eventtickets/exceptions/GlobalException.java
+++ src/main/java/com/thegym/eventtickets/exceptions/GlobalException.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +13,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalException {
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Validation error: ", ex);
+
+        ErrorDto errorDto = new ErrorDto();
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream().findFirst()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .orElse("Validation error occurred");
+        errorDto.setError(errorMessage);
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<ErrorDto> handleConstraintViolation(
             ConstraintViolationException ex
